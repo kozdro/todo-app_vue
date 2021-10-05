d6dc3e2e7ba173188db9a4b3c4c078b7fda8508b
Bartek Kozdrowiecki <bkozdrowiecki@divante.pl>
2021-10-05 15:39:39 +0200

    backend: task2

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,63 @@
 // Backend ExpressJs app
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(8080)
+
+const mongo = require('mongodb')
+const MongoClient = mongo.MongoClient
+const uri = 'mongodb+srv://user:user@cluster0.pl6it.mongodb.net/todoVue?retryWrites=true&w=majority'
+let client
+
+const mongoClient = new MongoClient(uri, {
+  useNewUrlParser: true
+})
+
+mongoClient.connect((err, db) => {
+  if (err !== null) {
+    console.log(err)
+
+    return
+  }
+  
+  client = db
+})
+
+app.post('/', (req, res) => {
+  const collection = client.db('todoVue').collection('todos')
+  
+  collection.insertOne({ description: , dueTo:  }, (err, results) => {
+    if (err) {
+      console.log(err)
+      res.send('')
+
+      return
+    }
+
+    res.send(results.ops[0])
+  })
+})
+
+app.post('/', (req, res) => {
+  const collection = client.db('todoVue').collection('todos')
+
+  collection.removeOne({ id: }, (err, results) => {
+    if (err) {
+      console.log(err)
+      res.send('')
+      
+      return
+    }
+
+    res.send()
+  })
+})
